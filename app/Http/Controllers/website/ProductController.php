9f6449e7098191c77d42f2e0a25d5edc8a2e0b60
yartanguy17 <tanguydepot@gmail.com>
2022-12-27 16:45:56 +0100

    tanguy update product controller

--- app/Http/Controllers/website/ProductController.php
+++ app/Http/Controllers/website/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\website;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Support\Facades\Crypt;
 
 class ProductController extends Controller
@@ -12,8 +13,9 @@ class ProductController extends Controller
     public function index($id){
         $ID = Crypt::decrypt($id);
         $product=Product::findOrFail($ID);
-        // dd($product);
-        return view('website.pages.product-detail',compact('product'));
+        $product_by_category=Product::where('cat_id',$product->cat_id)->get();
+        // dd($product_by_category);
+        return view('website.pages.product-detail',compact('product','product_by_category'));
     }
     public function getAlimentation(){
 
